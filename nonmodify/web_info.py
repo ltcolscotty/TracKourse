195903d900e76656a1c1efe0eaac1d4d4bb7cd48
MidnightChaos <aidan.yung@gmail.com>
2024-10-12 21:04:42 -0700

    working on workflow

--- nonmodify/web_info.py
+++ nonmodify/web_info.py
@@ -1,5 +1,18 @@
 def access_page():
+    """clear search bars and search the class input"""
     pass
 
-def scan_results():
+
+def scan_boxes():
+    """Look through the page and scan through divs"""
+    pass
+
+
+def agg_data():
+    """Process one box input into an analyzable dataform"""
+    pass
+
+
+def next_page():
+    """Iterate to next page if there is a next page"""
     pass
