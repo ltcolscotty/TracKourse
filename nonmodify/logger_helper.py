0cf464b95cb135bde28fb240800304694985e6e6
MidnightChaos <Aidan.yung@gmail.com>
2024-10-22 12:54:25 -0700

    added log file function to help with troubleshooting

---
+++ nonmodify/logger_helper.py
@@ -0,0 +1,9 @@
+def write_file(filename, content):
+    """Writes output to specified file
+    Args:
+        fileName: str - name of file to write to
+        content: str - file content
+    """
+    with open(filename, 'w') as file:
+        file.write(content)
+    print(f"Content written to {filename}")
