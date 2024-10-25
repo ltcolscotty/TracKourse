33ed4c2f492ec6ae869ab49d8342df36e8141e83
MidnightChaos <aidan.yung@gmail.com>
2024-10-24 20:14:44 -0700

    working full browser mode

--- v2_workbench.py
+++ v2_workbench.py
@@ -22,9 +22,12 @@ for specification in cc.class_list:
 with sync_playwright() as p:
     # Launch the browser
     browser = p.chromium.launch(
-        headless=False, channel="chrome", args=["--start-maximized"]
+        headless=False,
+        channel="chrome",
+        args=["--start-maximized"]
     )
-    page = browser.new_page()
+    context = browser.new_context(no_viewport=True)
+    page = context.new_page()
 
     # Navigate to the constructed URL
     url = url_list[2]
@@ -32,6 +35,8 @@ with sync_playwright() as p:
 
     result = wi2.scan_boxes(page)
     lh.write_file("v2test.txt", result)
+
+    input("Press Enter to close the browser...")
 
     # Close the browser
     browser.close()
