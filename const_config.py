804dc20f4fdea6cf887dc81652acc18851a80421
MidnightChaos <aidan.yung@gmail.com>
2024-10-27 21:29:29 -0700

    removed most for testing purposes

--- const_config.py
+++ const_config.py
@@ -30,22 +30,6 @@ url_year = 2251  # configure based on current year url (eg. 2025 spring is 2551,
 # Add or delete class entries as needed. If you have a specific hybrid course you are looking for, specify the ID, it is a feasible workaround for filtering
 class_list = [
     class_info(
-        "MAT",
-        "267",
-        professor_list=["Eric Kostelich", "Ryan Frier", "zdzislaw Jackwicz"],
-        location="TEMPE",
-        start="9:00AM",
-        end="5:00PM",
-    ),
-    class_info(
-        "MAT",
-        "243",
-        professor_list=["Ajith Nair", "Haiyan Wang", "Hedvig Mohacsy"],
-        location="TEMPE",
-        start="9:00AM",
-        end="5:00PM",
-    ),
-    class_info(
         "ENG",
         "102",
         professor_list=["William Martin", "Brian Bender"],
@@ -53,23 +37,6 @@ class_list = [
         start="9:00AM",
         end="5:00PM",
         id_list=["12974", "10164", "10154", "19917", "23061", "25462"],
-    ),
-    class_info(
-        "CSE",
-        "240",
-        professor_list=["David Claveau", "James Gordon", "Justin Selgrad"],
-        location="TEMPE",
-        start="9:00AM",
-        end="5:00PM",
-    ),
-    class_info(
-        "GRK",
-        "142",
-        professor_list=["Sarah Bolmarcich"],
-        location="ICOURSE",
-        start="9:00AM",
-        end="5:00PM",
-        id_list=["13837"],
     ),
 ]
 
