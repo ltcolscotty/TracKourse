24ce4f9d8c2031ca184d459ae6a17fede8a8a038
MidnightChaos <aidan.yung@gmail.com>
2024-10-28 22:29:57 -0700

    removed unnessecary tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,13 +1,4 @@
 import src.trackourse.nonmodify.process_classes as pc
-import src.trackourse.nonmodify.class_info as ci
-
-
-def test_is_after():
-    assert pc.isAfter("4:00 AM", ci.convert_time("12:01AM")) == True
-
-
-def test_is_before():
-    assert pc.isBefore("9:00 PM", ci.convert_time("11:00PM")) == True
 
 
 def test_standardize_hybrid():
