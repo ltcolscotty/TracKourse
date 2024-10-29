19547ffa928b334a65d959337d259eb0cdd14b31
MidnightChaos <aidan.yung@gmail.com>
2024-10-28 19:36:45 -0700

    formatted tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -2,7 +2,6 @@ import src.trackourse.nonmodify.process_classes as pc
 import src.trackourse.nonmodify.class_info as ci
 
 
-
 def test_is_after():
     assert pc.isAfter("4:00 AM", ci.convert_time("12:01AM")) == True
 
@@ -51,23 +50,32 @@ Tempe - WXLR555
 Internet - Hybrid
 10 of 15"""
 
-    assert pc.standardize_hybrid(testcase1) == """AAA 800
+    assert (
+        pc.standardize_hybrid(testcase1)
+        == """AAA 800
 12345
 Jeffery De
 Hybrid | 11:00 AM - 12:15 PM
 10 of 15"""
+    )
 
-    assert pc.standardize_hybrid(testcase2) == """BBB 555
+    assert (
+        pc.standardize_hybrid(testcase2)
+        == """BBB 555
 54321
 Jane Doe
 Hybrid | 3:00 AM - 7:15 AM
 10 of 15"""
+    )
 
-    assert pc.standardize_hybrid(testcase3) == """CCC 100
+    assert (
+        pc.standardize_hybrid(testcase3)
+        == """CCC 100
 99999
 Alice Bob, Bob Alice
 Hybrid | 2:00 PM - 5:15 PM
 10 of 15"""
+    )
 
 
 def test_standardize_reg():
@@ -92,18 +100,27 @@ T Th | 1:30 PM - 2:45 PM
 Tempe - DISCVRY
 1 of 15"""
 
-    assert pc.standardize_reg(testcase1) == """YYY 999
+    assert (
+        pc.standardize_reg(testcase1)
+        == """YYY 999
 10101
 Jffery Doe
 M W F | 12:20 PM - 1:10 PM
 5 of 15"""
-    assert pc.standardize_reg(testcase2) == """ZZZ 101
+    )
+    assert (
+        pc.standardize_reg(testcase2)
+        == """ZZZ 101
 20202
 Jane Doe
 T Th | 3:00 PM - 4:15 PM
 13 of 15"""
-    assert pc.standardize_reg(testcase3) == """XXX 700
+    )
+    assert (
+        pc.standardize_reg(testcase3)
+        == """XXX 700
 30303
 Alice Bob, Bob Alice
 T Th | 1:30 PM - 2:45 PM
 1 of 15"""
+    )
