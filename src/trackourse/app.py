cadb2a066f901221b1ee90ae065c82d21c588af2
MidnightChaos <Aidan.yung@gmail.com>
2025-08-26 14:06:09 -0700

    blur doesn't seem to work, need to find another method

--- src/trackourse/app.py
+++ src/trackourse/app.py
@@ -2,26 +2,23 @@ import sys
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 
-from BlurWindow.blurWindow import blur
+from BlurWindow.blurWindow import GlobalBlur
 
 import sys
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QVBoxLayout()
+        self.resize(500, 400)
+
+        GlobalBlur(self.winId(), hexColor=False, Dark=True, Acrylic=False,QWidget=self)
 
         self.setWindowTitle("TracKourse")
         # button = QPushButton("Ding")
         # self.setCentralWidget(button)
         self.setFixedSize(QSize(400, 300))
         self.setWindowOpacity(0.8)
-        self.blur = QGraphicsBlurEffect()
-        self.blur.setBlurRadius(10)
-        self.setGraphicsEffect(self.blur)
-
-        self.setLayout(layout)
 
 app = QApplication(sys.argv)
 window = MainWindow()
