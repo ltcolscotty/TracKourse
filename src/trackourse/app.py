e8034287c06490b4cd2683cc9ae9ff8ebcd6446c
MidnightChaos <Aidan.yung@gmail.com>
2025-08-26 11:38:42 -0700

    blur!

--- src/trackourse/app.py
+++ src/trackourse/app.py
@@ -1,17 +1,17 @@
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QBoxLayout, QLineEdit, QWidget
 
 import sys
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("TracKourse")
-        button = QPushButton("Ding")
-
-        self.setCentralWidget(button)
+        # button = QPushButton("Ding")
+        # self.setCentralWidget(button)
+        self.setFixedSize(QSize(400, 300))
+        self.setWindowOpacity(0.8)
 
 app = QApplication(sys.argv)
 
