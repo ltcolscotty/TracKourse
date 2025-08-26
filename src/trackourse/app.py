36b07d019723ad02dd0ee0ad1e94feaf406d3e40
MidnightChaos <Aidan.yung@gmail.com>
2025-08-26 11:17:04 -0700

    a crippled little ratta

---
+++ src/trackourse/app.py
@@ -0,0 +1,21 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("TracKourse")
+        button = QPushButton("Ding")
+
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
