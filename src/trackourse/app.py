1983364325cdfec8d3388900ef063d01a40d8a6e
MidnightChaos <Aidan.yung@gmail.com>
2025-08-26 19:15:44 -0700

    working on frontend helpers

--- src/trackourse/app.py
+++ src/trackourse/app.py
@@ -2,16 +2,26 @@ import sys
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.resize(500, 400)
 
+        class_code_label = QLabel(text="Class Code")
+        class_code_field = QLineEdit()
+        class_code_submit = QPushButton("Submit")
+
+        layout = QGridLayout(self)
+
+        layout.addWidget(class_code_label, 1, 1)
+        layout.addWidget(class_code_field, 1, 2)
+        layout.addWidget(class_code_submit, 1, 3)
+
         self.setWindowTitle("TracKourse")
         # self.setWindowIcon('WillGetToLater')
 
-        button = QPushButton("Ding")
-        self.setCentralWidget(button)
+        # button = QPushButton("Ding")
+        # self.setCentralWidget(button)
         self.setFixedSize(QSize(400, 300))
         self.setWindowOpacity(0.95)
 
---
+++ src/trackourse/nonmodify/frontend_helper.py
@@ -0,0 +1,32 @@
+from web_info import *
+
+def verify_class(id: str) -> bool:
+    """
+    Search to make sure class ID Exists
+
+    Args:
+        - id - class ID
+
+    Returns:
+        - Bool - Whether class exists or not
+    """
+    pass
+
+def get_current_classes():
+    pass
+
+def add_class(id: str):
+
+    pass
+
+def remove_class(id: str):
+    """
+    Removes class from ini file if it exists
+    """
+    pass
+
+def start_notifier():
+    pass
+
+def stop_notifier():
+    pass
